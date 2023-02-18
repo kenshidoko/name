e427b9c5bc9617971becc34d9726937402e06a82
haris17890 <96570537+haris17890@users.noreply.github.com>
2023-02-18 08:42:43 +0800

    Add files via upload

---
+++ All Of Us Are Dead.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 9759729519 or 10950394697 or 10950541730 then
+    for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
+v:Disable()
+end
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/haris17890/name/main/Protected.lua"))()
+    end
